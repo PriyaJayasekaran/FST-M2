9d6e2453b794216d9428ae56d0c8199c44a063be
PriyaJayasekaran <40622066+PriyaJayasekaran@users.noreply.github.com>
2021-07-14 11:53:46 +0530

    Add files via upload
    JUnit activities

---
+++ junit/TestProgram.java
@@ -0,0 +1,57 @@
+package activities;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class TestProgram {
+
+	 @Test
+		
+	    void testZ() {
+		
+	        assertEquals(2, 1 + 1);
+		
+	    }
+		 
+		
+	    @Test
+		
+	    void testA() {
+		
+	        assertEquals(2, 1 + 1);
+		
+	    }
+		
+	 
+		
+	    @Test
+		
+	    void testY() {
+		
+	        assertEquals(2, 1 + 1);
+		
+	    }
+		
+	 
+		
+	    @Test
+		
+	    void testE() {
+		
+	        assertEquals(2, 1 + 1);
+		
+	    }
+		
+	 
+		
+	    @Test
+		
+	    void testB() {
+		
+	        assertEquals(2, 1 + 1);
+		
+	    }
+	
+	
+}
